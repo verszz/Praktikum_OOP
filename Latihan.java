776eb1b2e3750688cf38809f3064ecc4a1e5bdf9
lowpow12 <distinctlow@gmail.com>
2023-09-07 07:46:40 +0700

    First Try

---
+++ Latihan.java
@@ -0,0 +1,20 @@
+
+public class Latihan
+{
+    public static void main (String [] args ){
+int a = 10;
+int b = 5;
+System.out.println("Isi variabel A adalah " + a);
+System.out.println("Isi variabel B adalah " + b);
+System.out.println("Hasil penjumlahan variabel A dan B = " + (a+b));
+System.out.println("Hasil pengurangan variabel A dan B = " + (a-b));
+System.out.println("Hasil perkalian variabel A dan B = " + (a*b));
+System.out.println("Hasil pembagian variabel A dan B = " + (a/b));
+}
+
+    
+  
+
+    
+    
+}
